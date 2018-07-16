148ba510c74e659b60eca0df4f96286a433b9a02
Yarik Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2018-07-16 18:16:56 +0300

    list

--- src/App.js
+++ src/App.js
@@ -1,42 +1,77 @@
 import React, {Component} from 'react';
 import NavBar from './NavBar/NavBar.js';
-import Client from './Client/Client.js';
+import Client from './Client/Client';
 import './App.css';
 
 class App extends Component {
     state = {
-        clients:[
-            {firstName:'Yarik', lastName:'Mokhurenko', email:'yarik335@rambler.ru'},
-            {firstName:'Yarik', lastName:'Mokhurenko', email:'yarik335@rambler.ru'},
-            {firstName:'Yarik', lastName:'Mokhurenko', email:'yarik335@rambler.ru'}
-        ]
+        clients: [
+            {id:'1', firstName: 'Yarik', lastName: 'Mokhurenko', email: 'yarik335@rambler.ru'},
+            {id:'2', firstName: 'Yarik', lastName: 'Mokhurenko', email: 'yarik335@rambler.ru'},
+            {id:'3', firstName: 'Yarik', lastName: 'Mokhurenko', email: 'yarik335@rambler.ru'}
+        ],
+        showClients: false
     };
-delete = () => {
-    this.setState({
-        clients:[
-            {firstName:'Yarik', lastName:'Mokhurenko', email:'yarik335@rambler.ru'},
-            {firstName:'Yarik', lastName:'Mokhurenko', email:'yaroslavmokhurenko@gmail.com'},
-            {firstName:'Yarik', lastName:'Mokhurenko', email:'yarik335@rambler.ru'}
-        ]
-    })
-};
+
+    nameChangedHandler = (event) => {
+        this.setState({
+            clients: [
+                {firstName: 'Yarik', lastName: 'Mokhurenko', email: 'yarik335@rambler.ru'},
+                {firstName: 'Yarik', lastName: 'Mokhurenko', email: event.target.value},
+                {firstName: 'Yarik', lastName: 'Mokhurenko', email: 'yarik335@rambler.ru'}
+            ]
+        })
+    };
+
+    toggleClients = () => {
+        const doesShow = this.state.showClients;
+        this.setState({showClients: !doesShow});
+    };
+deleteClientHandler = (clientIndex) => {
+
+    // const  clients = this.state.clients.slice();
+    const  clients = [...this.state.clients];
+    clients.splice({clients:clients});
+    this.setState({clients: clients})
+}
 
     render() {
+        const style = {
+            backgroundcolor: 'white',
+            font: 'inherit',
+            boreder: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+        let clients = null;
+        if (this.state.showClients) {
+            clients = (
+                <div className="table-row header">
+                    {this.state.clients.map((client,index) => {
+                        return <Client click={this.deleteClientHandler(index)}
+                            firstName={client.firstName}
+                                       lastName={client.lastName}
+                                       email={client.email}
+                                       key={client.id}
+                        />
+                    })}
+
+                </div>
+            );
+
+        }
         return (
             <div className="App">
                 <NavBar/>
                 <div className="container ">
-                    <div className="table-row header">
-                        <Client firstName={this.state.clients[0].firstName}
-                                lastName={this.state.clients[0].lastName}
-                                click={this.delete}
-                                email={this.state.clients[0].email}/>
-                        <Client firstName={this.state.clients[1].firstName} lastName={this.state.clients[1].lastName} email={this.state.clients[1].email}>hobbies: racing</Client>
-                        <Client firstName={this.state.clients[2].firstName} lastName={this.state.clients[2].lastName} email={this.state.clients[2].email}/>
-                        <button type="button" className="close text-right" onClick={this.delete} aria-label="Close" id="delete-client">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
+                    {clients}
+
+                    <button type="button" style={style} className="close text-right"
+                            onClick={this.toggleClients} aria-label="Close"
+                            id="delete-client">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+
                 </div>
             </div>
         );
--- src/Client/Client.js
+++ src/Client/Client.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
+import './Client.css'
+
 const client = (props) => {
     return (
-        <div className="client row">
+        <div className="Client  ">
             <p id="client-id"> 1 </p>
             <p id="client-name"> {props.firstName} </p>
             <p id="client-last-name"> {props.lastName} </p>
             <p id="client-email" onClick={props.click}> {props.email} </p>
             {/*<p className="text-right">*/}
-                {/*<button type="button" className="close text-right" aria-label="Close" id="delete-client">*/}
-                    {/*<span aria-hidden="true">&times;</span>*/}
-                {/*</button>*/}
+            {/*<button type="button" className="close text-right" aria-label="Close" id="delete-client">*/}
+            {/*<span aria-hidden="true">&times;</span>*/}
+            {/*</button>*/}
             {/*</p>*/}
+            <input type="text" onChange={props.changed} value={props.email}/>
         </div>
     )
 };
